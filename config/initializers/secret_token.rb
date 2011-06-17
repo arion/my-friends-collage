8c170a2356c031693ab4811cf3d8e1a84c101ddf
Arion <mearion@gmail.com>
2011-06-17 10:24:07 +0400

    init empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFriendsColage::Application.config.secret_token = '9f69d435cfbb4344fedd90affa7c126e05fca5c694b0a9a188a3bb05c72ce2142dd9a5be608e1046fa02487dd3cc680fed55b44cba32c5552cc1274235def580'
